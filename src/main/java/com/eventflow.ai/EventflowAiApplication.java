e0d062479f707008077f826027467dc850215d0e
gladythomas <gladychaprath1999@gmail.com>
2026-09-23 21:45:56 +0530

    chore: initialize EventFlow AI project

---
+++ src/main/java/com/eventflow.ai/EventflowAiApplication.java
@@ -0,0 +1,13 @@
+package com.eventflow_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventflowAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventflowAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventflow_ai/EventflowAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventflow_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventflowAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
